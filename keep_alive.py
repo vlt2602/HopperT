4feaf5c8a3689db0fd5683345b7f50c420b1bfd2
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-05-23 23:54:21 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,16 @@
+# flask_app.py
+
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
-
+app = Flask("HopperT")
 
 @app.route('/')
 def home():
-    return "✅ HopperX đang hoạt động!"
-
+    return "✅ HopperT đang hoạt động ổn định!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run)
