3ef2a148e7a85e7f019b96b22cdbb20581d739f0
vlt2602 <155764244+vlt2602@users.noreply.github.com>
2025-05-24 23:55:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -53,13 +53,3 @@ if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
     threading.Thread(target=run_scheduler_safe).start()
     asyncio.run(run_async_tasks())
-
-import nest_asyncio
-import asyncio
-from telegram_handler import start_telegram_bot
-
-# Kích hoạt hỗ trợ vòng lặp lồng nhau
-nest_asyncio.apply()
-
-# Khởi chạy bot Telegram
-asyncio.get_event_loop().run_until_complete(start_telegram_bot())
